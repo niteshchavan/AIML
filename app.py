f04b723c677655a48566e97f22538b02d288789f
niteshchavan <nitesh.chavan@outlook.com>
2024-07-27 00:59:01 +0530

    main

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_models import ChatOllama
 
+
+
 chat_message_history = SQLChatMessageHistory(
     session_id="1", connection_string="sqlite:///sqlite.db"
 )
@@ -39,14 +41,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('chat2.html')
+
 
 
 @app.route('/query', methods=['POST'])
 def query():
     data = request.get_json()
     query_text = data.get("query_text", "")
-
+    print(query_text)
     #chat_message_history.add_user_message(query_text)
     #chat_message_history.add_ai_message("Hi")
     config = {"configurable": {"session_id": "1"}}
@@ -56,9 +59,24 @@ def query():
 
 
     messages = chat_message_history.messages
-    print(messages, "\n\n")
+    result = [doc.content for doc in messages]
+    last_10_results = result[-1:]
 
-    return jsonify({'results': messages}), 200
+    structured_messages = []
+
+    # Iterate over the result list and alternate between Human and AI
+    for i, message in enumerate(last_10_results):
+        if i % 2 == 0:
+            speaker = "Human"
+        else:
+            speaker = "AI"
+        
+        structured_messages.append({"speaker": speaker, "message": message})
+
+    print(structured_messages)
+
+  
+    return jsonify(structured_messages), 200
 
 
 
