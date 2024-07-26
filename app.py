80cc49c5a9690dc64c3f8ca4d181c6b79d2eb893
niteshchavan <nitesh.chavan@outlook.com>
2024-07-26 15:48:02 +0530

    main

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from langchain_community.document_loaders import AsyncChromiumLoader
-from langchain_community.document_transformers import BeautifulSoupTransformer
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
+from bs4 import BeautifulSoup as Soup
+from flask import Flask, request, render_template, jsonify
 
+import re
+import os
 
-#a list of urls
-urls = ["https://langchain-ai.github.io/langgraph/"]
+app = Flask(__name__)
 
-#initialize document loader
-loader = AsyncChromiumLoader(urls, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-docs = loader.load()
-print(docs)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+chroma_db = 'chromadb'
 
-bs_transformer = BeautifulSoupTransformer()
-docs_transformed = bs_transformer.transform_documents(
-        docs, tags_to_extract=["span"])
-splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=1000, chunk_overlap=0
+url = "https://www.pitambari.com/"
+loader = RecursiveUrlLoader(
+    url=url, max_depth=2, extractor=lambda x: Soup(x, "html.parser").text
 )
+docs = loader.load()
+print(len(docs))
 
-splits = splitter.split_documents(docs_transformed)
+#print(docs)
+#print(docs[0].page_content)
 
-schema = {
-    "properties": {
-        "news_article_title": {"type": "string"},
-        "news_article_summary": {"type": "string"},
-    },
-    "required": ["news_article_title", "news_article_summary"],
-}
+def clean_text(text):
+    return re.sub(r'\n+', ' ', text).strip()
 
-print(splits)
-#extracted_content = extract(schema=schema, content=splits[0].page_content)
+#cleaned_docs = clean_text(docs.page_content)
+#print(cleaned_docs)
 
+results = clean_text([doc.page_content for doc in docs])
